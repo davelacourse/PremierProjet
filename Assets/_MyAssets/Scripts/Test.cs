2e35b6eb3253e219895fcff715baa816b7950866
ProfDave <dave.lacourse@cegeptr.qc.ca>
2026-08-28 14:40:25 -0400

    Fin du cours #2
    gestion parent, scripts

---
+++ Assets/_MyAssets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int age = 25;
+    
+    private void Start()
+    {
+       // Debug.Log("Test de fonctionnement");
+       //  Debug.Log (age);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Test de fonctionnement");
+    }
+}
